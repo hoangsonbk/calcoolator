780d1a039f1568dd0515966d1fbab016b0e6f97e
Son Nguyen <son.nguyen@quoine.com>
2018-05-19 15:40:57 +0900

    initial

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './App.css';
+
+const App = () => {
+  return (
+    <div className={styles.body}>
+      Hello
+    </div>
+  );
+}
+
+export default App;
