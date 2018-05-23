f68a8d69deca1eb8fc32f785e69bbb1d810eb8fc
Son Nguyen <son.nguyen@quoine.com>
2018-05-23 23:26:54 +0900

    update color

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -12,31 +12,31 @@ const App = () => {
           <Keypad value="C" color="color1" />
           <Keypad value="+/-" color="color1" />
           <Keypad value="%" color="color1" />
-          <Keypad value="+" color="color1" />
+          <Keypad value="÷" color="color1" />
         </div>
         <div className={styles.row}>
           <Keypad value="1" color="color3" />
           <Keypad value="2" color="color3" />
           <Keypad value="3" color="color3" />
-          <Keypad value="−" color="color1" />
+          <Keypad value="×" color="color1" />
         </div>
         <div className={styles.row}>
           <Keypad value="4" color="color3" />
           <Keypad value="5" color="color3" />
           <Keypad value="6" color="color3" />
-          <Keypad value="×" color="color1" />
+          <Keypad value="−" color="color1" />
         </div>
         <div className={styles.row}>
           <Keypad value="7" color="color3" />
           <Keypad value="8" color="color3" />
           <Keypad value="9" color="color3" />
-          <Keypad value="÷" color="color1" />
+          <Keypad value="+" color="color1" />
         </div>
         <div className={styles.row}>
-          <Keypad value="0" color="color2" />
-          <Keypad value="00" color="color2" />
-          <Keypad value="." color="color2" />
-          <Keypad value="=" color="color2" />
+          <Keypad value="0" color="color3" className={styles.botLeft} />
+          <Keypad value="00" color="color3" />
+          <Keypad value="." color="color3" />
+          <Keypad value="=" color="color2" className={styles.botRight} />
         </div>
       </div>
     </div>
--- src/components/Keypad/Keypad.jsx
+++ src/components/Keypad/Keypad.jsx
@@ -2,8 +2,8 @@ import  React from 'react';
 
 import styles from './Keypad.css';
 
-const Keypad = ({ value, color }) => {
-  const styleName = `${styles.body} ${styles[color]}`
+const Keypad = ({ value, color, className }) => {
+  const styleName = `${styles.body} ${styles[color]} ${className}`;
   return (
     <div className={styleName}>
       <span className={styles.symbol}>
